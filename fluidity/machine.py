ed9bd366e79e03d4ae770d0eee71403d8e01962e
Rodrigo Manhães <rmanhaes@gmail.com>
2011-05-02 23:02:58 -0300

    state machine requires at least two states

--- fluidity/machine.py
+++ fluidity/machine.py
@@ -1,3 +1,9 @@
 class StateMachine(object):
+    def __init__(self):
+        if not getattr(self, 'states', None) or len(self.states) < 2:
+            raise InvalidConfiguration('There must be at least two states')
+
+
+class InvalidConfiguration(Exception):
     pass
 
--- spec/fluidity_spec.py
+++ spec/fluidity_spec.py
@@ -1,6 +1,6 @@
 import unittest
-from should_dsl import should, should_not, matcher
-from fluidity.machine import StateMachine
+from should_dsl import should
+from fluidity.machine import StateMachine, InvalidConfiguration
 
 
 class FluiditySpec(unittest.TestCase):
@@ -15,6 +15,17 @@ class FluiditySpec(unittest.TestCase):
     def it_has_an_initial_state(self):
         class MyMachine(StateMachine):
             initial_state = 'closed'
+            states = ['open', 'closed']
         machine = MyMachine()
         machine.initial_state |should| equal_to('closed')
+
+    def it_requires_at_least_two_states(self):
+        class MyMachine(StateMachine):
+            pass
+        MyMachine |should| throw(InvalidConfiguration,
+            message="There must be at least two states")
+        class OtherMachine(StateMachine):
+            states = ['open']
+        OtherMachine |should| throw(InvalidConfiguration,
+            message="There must be at least two states")
 
