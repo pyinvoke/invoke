6bf3ca816e91aeea6aafa57595ffc17ff9fd8e5e
Jeff Forcier <jeff@bitprophet.org>
2012-02-29 17:51:32 -0800

    Initial setup.py + README + cli stub

---
+++ invoke/cli.py
@@ -0,0 +1,2 @@
+def main():
+    raise NotImplementedError
---
+++ setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+# Support setuptools or distutils
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Import ourselves for version info
+import invoke
+
+# Frankenstein long_description: version-specific changelog note + README
+v = invoke.__version__
+long_description = """
+To find out what's new in this version of Invoke, please see `the changelog
+<http://docs.pyinvoke.org/en/%s/changelog.html>`_.
+
+%s
+""" % (v, open('README').read())
+
+setup(
+    name='invoke',
+    version=v,
+    description='Pythonic task execution',
+    license='BSD',
+
+    long_description=long_description,
+    author='Jeff Forcier',
+    author_email='jeff@bitprophet.org',
+    url='http://pyinvoke.org',
+
+    packages=["invoke"],
+    entry_points={
+        'console_scripts': [
+            'invoke = invoke.cli:main',
+        ]
+    },
+
+    classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Unix',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Build Tools',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: System :: Software Distribution',
+          'Topic :: System :: Systems Administration',
+    ],
+)
