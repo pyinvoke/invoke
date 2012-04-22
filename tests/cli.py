2034c8280800291227232435786441bfb0edace0
Jeff Forcier <jeff@bitprophet.org>
2012-04-22 15:15:56 -0700

    Add common CLI invocation test stubs.
    Doesn't go into positional args.

--- tests/cli.py
+++ tests/cli.py
@@ -1,6 +1,6 @@
 import os
 
-from spec import eq_
+from spec import eq_, skip
 
 from invoke import run
 
@@ -23,3 +23,47 @@ class CLI(object):
         # Doesn't specify --collection
         result = run("invoke foo")
         eq_(result.stdout, "Hm\n")
+
+    def boolean_args(self):
+        cmd = "taskname --boolean"
+        skip()
+
+    def flag_then_space_then_value(self):
+        cmd = "taskname --flag value"
+        skip()
+
+    def flag_then_equals_sign_then_value(self):
+        cmd = "taskname --flag=value"
+        skip()
+
+    def short_boolean_flag(self):
+        cmd = "taskname -f"
+        skip()
+
+    def short_flag_then_space_then_value(self):
+        cmd = "taskname -f value"
+        skip()
+
+    def short_flag_then_equals_sign_then_value(self):
+        cmd = "taskname -f=value"
+        skip()
+
+    def short_flag_with_adjacent_value(self):
+        cmd = "taskname -fvalue"
+        skip()
+
+    def flag_value_then_task(self):
+        cmd = "task1 -f notatask task2"
+        skip()
+
+    def flag_value_same_as_task_name(self):
+        cmd = "task1 -f mytask mytask"
+        skip()
+
+    def complex_multitask_invocation(self):
+        cmd = "-c integration task1 --bool_arg --val_arg=value task2 --val_arg othervalue"
+        skip()
+
+    def three_tasks_with_args(self):
+        cmd = "task1 --task1_bool task2 --task2_arg task2_arg_value task3"
+        skip()
