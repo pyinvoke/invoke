2b12f26b147690fe054694afba8de09fa095cad4
Jeff Forcier <jeff@bitprophet.org>
2012-05-09 08:54:29 -0700

    Yea explicit boolean tests are pretty dumb aren't they

--- tests/argument.py
+++ tests/argument.py
@@ -8,28 +8,28 @@ class Argument_(Spec):
         names = ('--foo', '-f')
         a = Argument(names=names)
         for name in names:
-            eq_(a.answers_to(name), True)
+            assert a.answers_to(name)
 
     def may_take_name_arg(self):
-        eq_(Argument(name='-b').answers_to('-b'), True)
+        assert Argument(name='-b').answers_to('-b')
 
     @raises(TypeError)
     def must_have_at_least_one_name(self):
         Argument()
 
     def defaults_to_not_needing_a_value(self):
-        eq_(Argument(name='a').needs_value, False)
+        assert not Argument(name='a').needs_value
 
     def may_specify_value_factory(self):
-        eq_(Argument(name='a', value=str).needs_value, True)
+        assert Argument(name='a', value=str).needs_value
 
     class answers_to:
         def returns_True_if_given_name_matches(self):
-            eq_(Argument(names=('--foo',)).answers_to('--foo'), True)
+            assert Argument(names=('--foo',)).answers_to('--foo')
 
     class needs_value:
         def returns_True_if_this_argument_needs_a_value(self):
-            eq_(Argument(name='-b', value=str).needs_value, True)
+            assert Argument(name='-b', value=str).needs_value
 
         def returns_False_if_it_does_not_need_a_value(self):
-            eq_(Argument(name='-b', value=None).needs_value, False)
+            assert not Argument(name='-b', value=None).needs_value
