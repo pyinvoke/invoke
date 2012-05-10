db6d354d3fd877ef41cffc1fe909c8e89d5f9e79
Jeff Forcier <jeff@bitprophet.org>
2012-05-10 09:05:49 -0700

    Make debug logging even more convenient.
    I'm sure we'll remove the globals() usage eventually.

--- invoke/parser/__init__.py
+++ invoke/parser/__init__.py
@@ -1,5 +1,6 @@
 from .context import Context
 from .argument import Argument # Mostly for importing via invoke.parser.<x>
+from ..util import debug
 
 
 class Parser(object):
@@ -13,12 +14,17 @@ class Parser(object):
         context = self.initial
         context_index = 0
         flag_index = 0
+        debug("Parsing argv %r" % (argv,))
+        debug("Starting with context %s" % context)
         for index, arg in enumerate(argv):
+            debug("Testing string arg %r at index %r" % (arg, index))
             if context.has_arg(arg):
                 flag_index = index
+                debug("Current context has this as a flag")
             # Otherwise, it's either a flag arg or a task name.
             else:
                 flag = argv[flag_index]
+                debug("Current context does not have this as a flag")
                 # If previous flag takes an arg, this is the arg
                 if context.needs_value(flag):
                     context.set_value(flag, arg)
@@ -26,4 +32,5 @@ class Parser(object):
                 else:
                     context = self.contexts[arg]
                     context_index = index
+                    debug("That pevious flag needs no value")
         return self.contexts
