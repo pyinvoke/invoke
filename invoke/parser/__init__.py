99811f85b734c3477b08a4395bd85efa7380629a
Jeff Forcier <jeff@bitprophet.org>
2012-05-10 09:07:35 -0700

    Lots of notes/tweaks to parser alg

--- invoke/parser/__init__.py
+++ invoke/parser/__init__.py
@@ -13,24 +13,37 @@ class Parser(object):
         # Assumes any program name has already been stripped out.
         context = self.initial
         context_index = 0
-        flag_index = 0
+        current_flag = None
         debug("Parsing argv %r" % (argv,))
         debug("Starting with context %s" % context)
         for index, arg in enumerate(argv):
             debug("Testing string arg %r at index %r" % (arg, index))
             if context.has_arg(arg):
-                flag_index = index
                 debug("Current context has this as a flag")
+                current_flag = context.get_arg(arg)
             # Otherwise, it's either a flag arg or a task name.
             else:
-                flag = argv[flag_index]
+                # TODO: this needs to be the Argument obj
                 debug("Current context does not have this as a flag")
                 # If previous flag takes an arg, this is the arg
-                if context.needs_value(flag):
-                    context.set_value(flag, arg)
-                # If not, it's the first task name
+                # TODO: this needs to test the Argument obj
+                if current_flag and current_flag.needs_value:
+                    debug("Previous flag needed a value, this is it")
+                    # TODO: type coercion? or should that happen on access
+                    # (probably yes)
+                    current_flag.set_value(arg)
+                # If not, it's the first task name (or invalid)
                 else:
-                    context = self.contexts[arg]
-                    context_index = index
                     debug("That pevious flag needs no value")
+                    # TODO: how best to associate task name with contexts?
+                    # (esp given a task can have N names)
+                    # TODO: brings in the usual question of the root ctx
+                    if arg in [x.name for x in self.contexts]:
+                        debug("Looks like a valid task name")
+                        context = self.contexts[arg]
+                        context_index = index
+                    else:
+                        # TODO: what error to raise?
+                        debug("Not a flag, a flag arg or a task: invalid")
+                        raise ValueError("lol")
         return self.contexts
