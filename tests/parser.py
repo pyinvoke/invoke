167bc3b36fe49ce0fbaaee4562de2b1a9bcac04e
Jeff Forcier <jeff@bitprophet.org>
2012-05-26 19:13:29 -0700

    Add more stub tests

--- tests/parser.py
+++ tests/parser.py
@@ -26,6 +26,14 @@ class Parser_(Spec):
     def raises_ValueError_for_unnamed_Contexts_in_contexts(self):
         Parser(initial=Context(), contexts=[Context()])
 
+    @raises(ValueError)
+    def raises_error_for_context_name_clashes(self):
+        skip()
+
+    @raises(ValueError)
+    def raises_error_for_context_alias_and_name_clashes(self):
+        skip()
+
     class parse_argv:
         def parses_sys_argv_style_list_of_strings(self):
             "parses sys.argv-style list of strings"
@@ -35,7 +43,16 @@ class Parser_(Spec):
             p = Parser(contexts=[mytask])
             p.parse_argv(['mytask', '--arg'])
 
-        def returns_ordered_list_of_tasks_and_their_args(self):
+        def returns_only_contexts_mentioned(self):
+            skip()
+
+        def returned_contexts_are_in_order_given(self):
+            skip()
+
+        def returned_context_member_arguments_contain_given_values(self):
+            skip()
+
+        def returned_arguments_not_given_contain_default_values(self):
             skip()
 
         def returns_remainder(self):
