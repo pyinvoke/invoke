2a2ee922921bbd233bc231983114d6aa1a1e92e1
Jeff Forcier <jeff@bitprophet.org>
2012-05-28 13:10:31 -0700

    Overhaul Argument spec

--- invoke/parser/argument.py
+++ invoke/parser/argument.py
@@ -1,17 +1,27 @@
 class Argument(object):
-    def __init__(self, name=None, names=(), value=None):
+    def __init__(self, name=None, names=(), kind=bool, default=None):
         if name and names:
             msg = "Cannot give both 'name' and 'names' arguments! Pick one."
             raise TypeError(msg)
         if not (name or names):
             raise TypeError("An Argument must have at least one name.")
         self.names = names if names else (name,)
-        self.value_factory = value
+        self.kind = kind
+        self.raw_value = self._value = None
+        self.default = default
 
     def __str__(self):
-        valfact = (" %r" % (self.value_factory)) if self.value_factory else ""
-        return "Arg: %r%s" % (self.names, valfact)
+        return "Arg: %r (%s)" % (self.names, self.kind)
 
     @property
-    def needs_value(self):
-        return self.value_factory is not None
+    def takes_value(self):
+        return self.kind is not bool
+
+    @property
+    def value(self):
+        return self._value if self._value is not None else self.default
+
+    @value.setter
+    def value(self, arg):
+        self.raw_value = arg
+        self._value = self.kind(arg)
--- tests/argument.py
+++ tests/argument.py
@@ -4,34 +4,76 @@ from invoke.parser import Argument
 
 
 class Argument_(Spec):
-    def may_take_names_list(self):
-        names = ('--foo', '-f')
-        a = Argument(names=names)
-        # herp a derp
-        for name in names:
-            assert name in a.names
+    class init:
+        "__init__"
+        def may_take_names_list(self):
+            names = ('--foo', '-f')
+            a = Argument(names=names)
+            # herp a derp
+            for name in names:
+                assert name in a.names
 
-    def may_take_name_arg(self):
-        assert '-b' in Argument(name='-b').names
+        def may_take_name_arg(self):
+            assert '-b' in Argument(name='-b').names
 
-    @raises(TypeError)
-    def must_have_at_least_one_name(self):
-        Argument()
+        @raises(TypeError)
+        def must_get_at_least_one_name(self):
+            Argument()
 
-    def defaults_to_not_needing_a_value(self):
-        assert not Argument(name='a').needs_value
+    class kind_kwarg:
+        "'kind' kwarg"
 
-    def may_specify_value_factory(self):
-        assert Argument(name='a', value=str).needs_value
+        def is_optional(self):
+            Argument(name='a')
+            Argument(name='b', kind=int)
+
+        def defaults_to_bool(self):
+            eq_(Argument('a').kind, bool)
+
+        def non_bool_implies_value_needed(self):
+            assert Argument(name='a', kind=int).takes_value
+
+        def bool_implies_no_value_needed(self):
+            assert not Argument(name='a', kind=bool).takes_value
+
+        @raises(ValueError)
+        def may_validate_on_set(self):
+            Argument('a', kind=int).value = 'five'
 
     class names:
         def returns_tuple_of_all_names(self):
             eq_(Argument(names=('--foo', '-b')).names, ('--foo', '-b'))
             eq_(Argument(name='--foo').names, ('--foo',))
 
-    class needs_value:
-        def returns_True_if_this_argument_needs_a_value(self):
-            assert Argument(name='-b', value=str).needs_value
+    class takes_value:
+        def False_for_basic_args(self):
+            assert not Argument(name='a').takes_value
 
-        def returns_False_if_it_does_not_need_a_value(self):
-            assert not Argument(name='-b', value=None).needs_value
+        def True_if_kind_is_set(self):
+            assert Argument(name='-b', kind=str).takes_value
+
+    class value_set:
+        "value="
+        def available_as_dot_raw_value(self):
+            "available as .raw_value"
+            a = Argument('a')
+            a.value = 'foo'
+            eq_(a.raw_value, 'foo')
+
+        def untransformed_appears_as_dot_value(self):
+            "untransformed, appears as .value"
+            a = Argument('a', kind=str)
+            a.value = 'foo'
+            eq_(a.value, 'foo')
+
+        def transformed_appears_as_dot_value_with_original_as_raw_value(self):
+            "transformed, modified value is .value, original is .raw_value"
+            a = Argument('a', kind=int)
+            a.value = '5'
+            eq_(a.value, 5)
+            eq_(a.raw_value, '5')
+
+    class value:
+        def returns_default_if_not_set(self):
+            a = Argument('a', default=25)
+            eq_(a.value, 25)
