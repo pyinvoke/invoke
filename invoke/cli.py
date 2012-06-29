f8f4b176685e83b0575e2794df44fd5ccf0ddc01
Jeff Forcier <jeff@bitprophet.org>
2012-06-28 19:14:49 -0700

    Formatting

--- invoke/cli.py
+++ invoke/cli.py
@@ -12,11 +12,13 @@ def parse(argv):
         Argument(names=('root', 'r'))
     ))
     # 'core' will result an .unparsed attribute with what was left over.
-    core = Parser(initial=initial_context, ignore_unknown=True).parse_argv(argv)
+    parser = Parser(initial=initial_context, ignore_unknown=True)
+    core = parser.parse_argv(argv)
     args = core[0].args
 
     # Load collection (default or specified) and parse leftovers
-    collection = Loader(root=args.root.value).load_collection(args.collection.value)
+    loader = Loader(root=args.root.value)
+    collection = loader.load_collection(args.collection.value)
     tasks = Parser(contexts=collection.to_contexts()).parse_argv(core.unparsed)
 
     # Now we can take action based on 'core' options and the 'tasks' found
