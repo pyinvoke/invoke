4b5bee34e14ac6b11442062807a8f287ce444570
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2012-07-14 17:58:26 -0400

    removing get method and updating __getitem__

--- invoke/collection.py
+++ invoke/collection.py
@@ -37,25 +37,11 @@ class Collection(object):
         """
         if not name:
             if self.default:
-                return self.get(self.default)
+                return self[self.default]
             else:
                 raise ValueError("This collection has no default task.")
         return self.tasks[name]
 
-    def get(self, name=None):
-        """
-        Returns task named ``name``. Honors aliases.
-
-        If this collection has a default task, it is returned when ``name`` is
-        empty or ``None``. If empty input is given and no task has been
-        selected as the default, ValueError will be raised.
-        """
-        if not name:
-            if self.default:
-                return self.get(self.default)
-            else:
-                raise ValueError("This collection has no default task.")
-        return self.tasks[name]
 
     def to_contexts(self):
         """
