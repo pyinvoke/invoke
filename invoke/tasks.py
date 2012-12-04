ab12e6f1fd4efa915d9c3b865b9f1b9cc47ef48d
Jeff Forcier <jeff@bitprophet.org>
2012-12-03 19:19:10 -0800

    comment WIP

--- invoke/tasks.py
+++ invoke/tasks.py
@@ -77,6 +77,9 @@ class Task(object):
         # getargspect()).
         tuples = []
         # Positionals first, removing from base list of arg names
+        # FIXME: WHY are positionals first?
+        # It messes up expected shortflags order now that positionals are flags
+        # again!
         for posarg in self.positional:
             tuples.append((posarg, spec_dict[posarg]))
             arg_names.remove(posarg)
