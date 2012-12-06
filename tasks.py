3721d4ae3a1b27a4b0bd6f1c489f2ce0600d3274
Jeff Forcier <jeff@bitprophet.org>
2012-12-05 18:40:02 -0800

    Port docs tasks from internal codebase

--- tasks.py
+++ tasks.py
@@ -1,3 +1,5 @@
+import os
+
 from invoke.tasks import task
 from invoke.runner import run
 
@@ -67,3 +69,26 @@ def vendorize(package, spec, git_url=None, license=None):
 @task
 def release():
     run("python setup.py sdist register upload")
+
+
+docs_dir = 'docs'
+build = os.path.join(docs_dir, '_build')
+
+
+@task
+def clean_docs():
+    run("rm -rf %s" % build)
+
+
+@task
+def browse_docs():
+    run("open %s" % os.path.join(build, 'index.html'))
+
+
+@task
+def docs(clean=False, browse=False):
+    if clean:
+        clean_docs.body()
+    run("sphinx-build %s %s" % (docs_dir, build), pty=True)
+    if browse:
+        browse_docs.body()
