b718838e699ac30ff5d731d9e3f8c8608473353b
Jeff Forcier <jeff@bitprophet.org>
2013-01-18 09:42:24 -0800

    Formatting/comment tweak

--- invoke/util.py
+++ invoke/util.py
@@ -2,12 +2,12 @@ import logging
 import os
 
 
-# Allow from-the-start debugging via shell env var
+# Allow from-the-start debugging (vs toggled during load of tasks module) via
+# shell env var
 if os.environ.get('INVOKE_DEBUG'):
     logging.basicConfig(level=logging.DEBUG)
 
-
-
+# Add top level logger functions to global namespace. Meh.
 log = logging.getLogger('invoke')
 for x in ('debug',):
     globals()[x] = getattr(log, x)
