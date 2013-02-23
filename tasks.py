09e8eda16907e63d71561aca7e598a831cdf9bcf
Jeff Forcier <jeff@bitprophet.org>
2013-02-23 15:17:44 -0800

    Override default docs with one that uses api_docs too

--- tasks.py
+++ tasks.py
@@ -1,4 +1,4 @@
-from invocations.docs import clean_docs, browse_docs, docs, api_docs
+from invocations.docs import clean_docs, browse_docs, docs as docs_, api_docs
 from invocations.testing import test
 from invocations.packaging import vendorize, release
 
@@ -8,3 +8,11 @@ from invoke import task, run
 @task
 def doctree():
     run("tree -Ca -I \".git|*.pyc|*.swp|dist|*.egg-info|_static|_build\" docs")
+
+# TODO: allow importing upstream 'docs' in non-binding fashion
+# TODO: before/after hooks
+@task
+def docs(clean=False, browse=False):
+    # Override clean to apply to API docs too
+    api_docs.body(target='invoke', exclude='invoke/vendor')
+    docs_.body(clean=clean, browse=browse)
