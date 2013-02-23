21f86a183463e708934b3ea99732a3263ee8ec6c
Donald Stufft <donald.stufft@gmail.com>
2013-02-23 00:56:39 -0500

    Fix encoding issues with a PTY

--- invoke/runner.py
+++ invoke/runner.py
@@ -109,7 +109,7 @@ def run(command, warn=False, hide=None, pty=False):
         hide = normalize_hide(hide)
         out = []
         def out_filter(text):
-            out.append(text.decode(sys.stdout.encoding))
+            out.append(text.decode("utf-8"))
             if 'out' not in hide:
                 return text
             else:
