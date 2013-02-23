4f9a49e6370f41cceadf89046196691ba8ff5834
Jeff Forcier <jeff@bitprophet.org>
2013-02-23 11:45:31 -0800

    Add doctree task

--- tasks.py
+++ tasks.py
@@ -1,3 +1,10 @@
 from invocations.docs import clean_docs, browse_docs, docs
 from invocations.testing import test
 from invocations.packaging import vendorize, release
+
+from invoke import task, run
+
+
+@task
+def doctree():
+    run("tree -Ca -I \".git|*.pyc|*.swp|dist|*.egg-info|_static|_build\" docs")
