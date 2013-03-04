0002abb76b4a7c4ab385957d1c1250fcb667aff8
Jeff Forcier <jeff@bitprophet.org>
2013-03-03 20:13:43 -0800

    Zomg leading dots in refs == best

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,9 @@ project = u'Invoke'
 year = datetime.now().year
 copyright = u'%d Jeff Forcier' % year
 
+# Ensure `links` try hitting API endpoints by default.
+default_role = 'py:obj'
+
 # Ensure project directory is on PYTHONPATH for version, autodoc access
 sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), '..')))
 
