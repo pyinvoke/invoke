d72f809ad44db45d96ea578f4c9f1c52716b43a7
Jeff Forcier <jeff@bitprophet.org>
2013-03-06 10:58:07 -0800

    Namespace-ify our own tasks module

--- tasks.py
+++ tasks.py
@@ -1,10 +1,14 @@
-from invocations.docs import clean_docs, browse_docs, docs
+from invocations import docs
 from invocations.testing import test
 from invocations.packaging import vendorize, release
 
-from invoke import task, run
+from invoke import task, run, Collection
 
 
 @task
 def doctree():
     run("tree -Ca -I \".git|*.pyc|*.swp|dist|*.egg-info|_static|_build\" docs")
+
+docs = Collection.from_module(docs)
+docs.add_task(doctree, 'tree')
+ns = Collection(test, vendorize, release, docs)
