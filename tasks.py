0ff6df104070b098ada7becb6b68275dd0360d4a
Jeff Forcier <jeff@bitprophet.org>
2013-03-16 15:31:34 -0700

    Vendorized pexpect-u + add convenience task for it

--- tasks.py
+++ tasks.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 from invocations import docs
 from invocations.testing import test
 from invocations.packaging import vendorize, release
@@ -9,6 +12,20 @@ from invoke import task, run, Collection
 def doctree():
     run("tree -Ca -I \".git|*.pyc|*.swp|dist|*.egg-info|_static|_build\" docs")
 
+@task
+def vendorize_pexpect(version):
+    target = 'invoke/vendor'
+    package = 'pexpect'
+    vendorize(
+        distribution="pexpect-u",
+        package=package,
+        version=version,
+        vendor_dir=target,
+        license='LICENSE', # TODO: autodetect this in vendorize
+    )
+    # Nuke test dir inside package hrrgh
+    shutil.rmtree(os.path.join(target, package, 'tests'))
+
 docs = Collection.from_module(docs)
 docs.add_task(doctree, 'tree')
-ns = Collection(test, vendorize, release, docs)
+ns = Collection(test, vendorize, release, docs, vendorize_pexpect)
