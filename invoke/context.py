cb19fc1b5fff56dd701920f50b9ef148cab36840
Jeff Forcier <jeff@bitprophet.org>
2013-04-14 12:04:55 -0700

    Failing test + stub behavior re: Context.run

--- invoke/context.py
+++ invoke/context.py
@@ -1,2 +1,17 @@
+from .runner import run
+
+
 class Context(object):
-    pass
+    """
+    Context-aware API wrapper & state-passing object.
+
+    Stores various bits of configuration state internally and uses them to set
+    default kwarg values for various API calls, such as `.run`.
+
+    See method call docstrings for additional details.
+    """
+    def __init__(self, run=None):
+        pass
+
+    def run(self, *args, **kwargs):
+        return run(*args, **kwargs)
--- tests/context.py
+++ tests/context.py
@@ -1,7 +1,12 @@
 from spec import Spec
+from mock import patch
 
 from invoke.context import Context
 
 
 class Context_(Spec):
-    pass
+    class run_:
+        @patch('invoke.context.run')
+        def honors_warn_state(self, run):
+            Context(run={'warn': True}).run('false')
+            run.assert_called_with('false', warn=True)
