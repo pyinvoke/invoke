5d189f008ae879625898c39484c3274226fadb2e
Ben Rousch <brousch@gmail.com>
2013-09-24 13:44:44 -0400

    Added Python3.2 and 3.3 to Programming Languages

--- setup.py
+++ setup.py
@@ -51,6 +51,8 @@ setup(
           'Programming Language :: Python',
           'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
           'Topic :: Software Development',
           'Topic :: Software Development :: Build Tools',
           'Topic :: Software Development :: Libraries',
