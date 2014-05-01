bd8a77131e3309b22ee775fd4fea031951bedccd
Jeff Forcier <jeff@bitprophet.org>
2014-05-01 15:41:09 -0700

    Restore utils stuff, is still useful for testing Collection in isolation

--- tests/_utils.py
+++ tests/_utils.py
@@ -1,4 +1,15 @@
-import os
+import os, sys
+from contextlib import contextmanager
 
 
 support = os.path.join(os.path.dirname(__file__), '_support')
+
+@contextmanager
+def support_path():
+    sys.path.insert(0, support)
+    yield
+    sys.path.pop(0)
+
+def load(name):
+    with support_path():
+        return __import__(name)
