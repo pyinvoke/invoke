739620388fb3988968e6a83adbb4aedadd1ff89d
Jeff Forcier <jeff@bitprophet.org>
2014-09-26 13:40:32 -0700

    Use newer attr access syntax for Executor config tests

--- tests/executor.py
+++ tests/executor.py
@@ -209,7 +209,7 @@ bar
         def hands_collection_configuration_to_context(self):
             @ctask
             def mytask(ctx):
-                eq_(ctx['my_key'], 'value')
+                eq_(ctx.my_key, 'value')
             c = Collection(mytask)
             c.configure({'my_key': 'value'})
             Executor(collection=c, context=Context()).execute('mytask')
@@ -217,10 +217,10 @@ bar
         def hands_task_specific_configuration_to_context(self):
             @ctask
             def mytask(ctx):
-                eq_(ctx['my_key'], 'value')
+                eq_(ctx.my_key, 'value')
             @ctask
             def othertask(ctx):
-                eq_(ctx['my_key'], 'othervalue')
+                eq_(ctx.my_key, 'othervalue')
             inner1 = Collection('inner1', mytask)
             inner1.configure({'my_key': 'value'})
             inner2 = Collection('inner2', othertask)
@@ -232,7 +232,7 @@ bar
         def subcollection_config_works_with_default_tasks(self):
             @ctask(default=True)
             def mytask(ctx):
-                eq_(ctx['my_key'], 'value')
+                eq_(ctx.my_key, 'value')
             # Sets up a task "known as" sub.mytask which may be called as just
             # 'sub' due to being default.
             sub = Collection('sub', mytask=mytask)
