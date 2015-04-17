0ca298f6706706637dccd4f27c56eed6e91c98ba
Jeff Forcier <jeff@bitprophet.org>
2015-04-16 20:18:16 -0400

    Rename new test class correctly and flesh out first passing tests

--- tests/runners.py
+++ tests/runners.py
@@ -1,13 +1,27 @@
-from spec import Spec
+import sys
+
+from spec import Spec, trap, eq_
+
+from invoke import Local, Context
+
+from _utils import mock_subprocess
 
 
-class Runner_(Spec):
+class Local_(Spec):
     class run:
+        @trap
+        @mock_subprocess(out="sup")
         def out_stream_defaults_to_sys_stdout(self):
             "out_stream defaults to sys.stdout"
+            Local(Context()).run("command")
+            eq_(sys.stdout.getvalue(), "sup")
 
+        @trap
+        @mock_subprocess(err="sup")
         def err_stream_defaults_to_sys_stderr(self):
             "err_stream defaults to sys.stderr"
+            Local(Context()).run("command")
+            eq_(sys.stderr.getvalue(), "sup")
 
         def out_stream_can_be_overridden(self):
             "out_stream can be overridden"
