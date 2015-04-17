1f268a485aeb2f8a3c3aecca04ba7e526d101e6b
Jeff Forcier <jeff@bitprophet.org>
2015-04-16 21:32:51 -0400

    Refactor a tad

--- tests/runners.py
+++ tests/runners.py
@@ -10,18 +10,21 @@ from _utils import mock_subprocess
 
 class Local_(Spec):
     class run:
+        def _run(self, *args, **kwargs):
+            return Local(Context()).run(*args, **kwargs)
+
         @trap
         @mock_subprocess(out="sup")
         def out_stream_defaults_to_sys_stdout(self):
             "out_stream defaults to sys.stdout"
-            Local(Context()).run("nope")
+            self._run("nope")
             eq_(sys.stdout.getvalue(), "sup")
 
         @trap
         @mock_subprocess(err="sup")
         def err_stream_defaults_to_sys_stderr(self):
             "err_stream defaults to sys.stderr"
-            Local(Context()).run("nope")
+            self._run("nope")
             eq_(sys.stderr.getvalue(), "sup")
 
         @trap
@@ -29,7 +32,7 @@ class Local_(Spec):
         def out_stream_can_be_overridden(self):
             "out_stream can be overridden"
             out = StringIO()
-            Local(Context()).run("nope", out_stream=out)
+            self._run("nope", out_stream=out)
             eq_(out.getvalue(), "sup")
             eq_(sys.stdout.getvalue(), "")
 
@@ -38,7 +41,7 @@ class Local_(Spec):
         def err_stream_can_be_overridden(self):
             "err_stream can be overridden"
             err = StringIO()
-            Local(Context()).run("nope", err_stream=err)
+            self._run("nope", err_stream=err)
             eq_(err.getvalue(), "sup")
             eq_(sys.stderr.getvalue(), "")
 
