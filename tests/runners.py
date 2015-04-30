370e93c8fca66cbd53556f1187ffce2d56e3f423
Jeff Forcier <jeff@bitprophet.org>
2015-04-30 14:00:43 -0700

    Move some tests from Local to Runner, they don't need to be Local specific anymore

--- tests/runners.py
+++ tests/runners.py
@@ -145,57 +145,51 @@ class Runner_(Spec):
         def honors_kwarg(self):
             skip()
 
-
-class Local_(Spec):
-    def _run(self, *args, **kwargs):
-        return Local(Context()).run(*args, **kwargs)
-
     class stream_control:
         @trap
-        @mock_subprocess(out="sup")
         def out_defaults_to_sys_stdout(self):
             "out_stream defaults to sys.stdout"
-            self._run("nope")
+            self._runner(out="sup").run("nope")
             eq_(sys.stdout.getvalue(), "sup")
 
         @trap
-        @mock_subprocess(err="sup")
         def err_defaults_to_sys_stderr(self):
             "err_stream defaults to sys.stderr"
-            self._run("nope")
+            self._runner(err="sup").run("nope")
             eq_(sys.stderr.getvalue(), "sup")
 
         @trap
-        @mock_subprocess(out="sup")
         def out_can_be_overridden(self):
             "out_stream can be overridden"
             out = StringIO()
-            self._run("nope", out_stream=out)
+            self._runner(out="sup").run("nope", out_stream=out)
             eq_(out.getvalue(), "sup")
             eq_(sys.stdout.getvalue(), "")
 
         @trap
-        @mock_subprocess(err="sup")
         def err_can_be_overridden(self):
             "err_stream can be overridden"
             err = StringIO()
-            self._run("nope", err_stream=err)
+            self._runner(err="sup").run("nope", err_stream=err)
             eq_(err.getvalue(), "sup")
             eq_(sys.stderr.getvalue(), "")
 
         @trap
-        @mock_pty(out="sup")
         def pty_defaults_to_sys(self):
-            self._run("nope", pty=True)
+            self._runner(out="sup").run("nope", pty=True)
             eq_(sys.stdout.getvalue(), "sup")
 
         @trap
-        @mock_pty(out="yo")
         def pty_out_can_be_overridden(self):
             out = StringIO()
-            self._run("nope", pty=True, out_stream=out)
+            self._runner(out="yo").run("nope", pty=True, out_stream=out)
             eq_(out.getvalue(), "yo")
             eq_(sys.stdout.getvalue(), "")
+
+
+class Local_(Spec):
+    def _run(self, *args, **kwargs):
+        return Local(Context()).run(*args, **kwargs)
 
     class pty_fallback:
         def warning_only_fires_once(self):
