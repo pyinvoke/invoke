8ae5c26f2ecb5750e9a06ac38035e38bfa45b0de
Jeff Forcier <jeff@bitprophet.org>
2015-09-18 15:20:57 -0700

    Port watch_tests to invocations

--- tasks.py
+++ tasks.py
@@ -2,21 +2,11 @@ import sys
 import time
 
 from invocations.docs import docs, www, sites, watch_docs
-from invocations.testing import test, coverage, integration
+from invocations.testing import test, coverage, integration, watch_tests
 from invocations.packaging import vendorize, release
 
 from invoke import ctask as task, Collection, Context
 from invoke.util import LOG_FORMAT
-
-
-@task
-def watch_tests(c, module=None):
-    """
-    Watch source tree and test tree for changes, rerunning tests as necessary.
-    """
-    watch(
-        c, test, ['\./invoke/', '\./tests/'], ['.*/\..*\.swp'], module=module
-    )
 
 
 ns = Collection(
@@ -24,6 +14,8 @@ ns = Collection(
     watch_docs, watch_tests
 )
 ns.configure({
-    'coverage': {'package': 'invoke'},
-    'tests': {'logformat': LOG_FORMAT},
+    'tests': {
+        'logformat': LOG_FORMAT,
+        'package': 'invoke',
+    },
 })
