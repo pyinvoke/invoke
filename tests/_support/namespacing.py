f0308afa12fe9c1bf9a5514dd05e2f94914e8629
Máté Farkas <mate.farkas@balabit.com>
2016-01-03 00:17:56 +0100

    Add missing __deepcopy__ to Call class
    Having __getattr__ without __deepcopy__ causes infinite
    recursion in Python 3 when you deepcopy that object. A test
    is also updated to cause the error fixed in this commit.
    
    Fixes #257
    
    Signed-off-by: Máté Farkas <mate.farkas@balabit.com>

--- tests/_support/namespacing.py
+++ tests/_support/namespacing.py
@@ -1,8 +1,12 @@
-from invoke import Collection, task
+from invoke import Collection, task, call
 
 from package import module
 
 @task
+def top_pre():
+    pass
+
+@task(call(top_pre))
 def toplevel():
     pass
 
