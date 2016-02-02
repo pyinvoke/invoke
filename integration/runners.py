a13244776b9017e8a339a94e0fe06d32fa61596d
Jeff Forcier <jeff@bitprophet.org>
2016-02-02 14:50:02 -0800

    Hide output from some tests

--- integration/runners.py
+++ integration/runners.py
@@ -39,8 +39,8 @@ class Runner_(Spec):
         def piped_stdin_is_not_conflated_with_mocked_stdin(self):
             # Re: GH issue #308
             # Will die on broken-pipe OSError if bug is present.
-            run("echo 'lollerskates' | inv -c nested_or_piped foo")
+            run("echo 'lollerskates' | inv -c nested_or_piped foo", hide=True)
 
         def nested_invoke_sessions_not_conflated_with_mocked_stdin(self):
             # Also re: GH issue #308. This one will just hang forever. Woo!
-            run("inv -c nested_or_piped calls_foo")
+            run("inv -c nested_or_piped calls_foo", hide=True)
