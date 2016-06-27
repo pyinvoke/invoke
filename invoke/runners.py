f55efff490f16dfae41e498ae6e2863754cebe00
Jeff Forcier <jeff@bitprophet.org>
2016-06-26 19:19:33 -0700

    Start generalizing prompt response stuff re #369.
    Created real-enough watcher API and started using it internally.
    
    Still needs surfacing at top of public API

--- invoke/runners.py
+++ invoke/runners.py
@@ -401,8 +401,12 @@ class Runner(object):
         # Determine pty or no
         self.using_pty = self.should_use_pty(opts['pty'], opts['fallback'])
         # Responses
-        # TODO: precompile the keys into regex objects
         self.responses = opts['responses']
+        if self.responses:
+            callbacks = []
+            for k, v in six.iteritems(self.responses):
+                callbacks.append(Responder(k, v))
+            self.responses = callbacks
         return opts, out_stream, err_stream, in_stream
 
     def generate_result(self, **kwargs):
@@ -665,35 +669,16 @@ class Runner(object):
 
         :returns: ``None``.
         """
-        # Short-circuit if there are no responses to respond to. This saves us
-        # the effort of joining the buffer and so forth.
-        if not self.responses:
-            return
-        # Join buffer contents into a single string; without this, we can't be
-        # sure that the pattern we seek isn't split up across chunks in the
-        # buffer.
+        # Join buffer contents into a single string; without this,
+        # StreamWatcher subclasses can't do things like iteratively scan for
+        # pattern matches.
         # NOTE: using string.join should be "efficient enough" for now, re:
-        # speed and memory use. Should that turn up false, consider using
-        # StringIO or cStringIO (tho the latter doesn't do Unicode well?)
-        # which is apparently even more efficient.
+        # speed and memory use. Should that become false, consider using
+        # StringIO or cStringIO (tho the latter doesn't do Unicode well?) which
+        # is apparently even more efficient.
         stream = u''.join(buffer_)
-        # Initialize seek indices
-        if not hasattr(indices, 'seek'):
-            indices.seek = {}
-            for pattern in self.responses:
-                indices.seek[pattern] = 0
-        for pattern, response in six.iteritems(self.responses):
-            # Only look at stream contents we haven't seen yet, to avoid dupes.
-            new_ = stream[indices.seek[pattern]:]
-            # Search, across lines if necessary
-            matches = re.findall(pattern, new_, re.S)
-            # Update seek index if we've matched
-            if matches:
-                indices.seek[pattern] += len(new_)
-            # Iterate over findall() response in case >1 match occurred.
-            for _ in matches:
-                # TODO: automatically append system-appropriate newline if
-                # response doesn't end with it, w/ option to disable?
+        for watcher in self.responses:
+            for response in watcher.submit(stream):
                 self.write_proc_stdin(response)
 
     def generate_env(self, env, replace_env):
@@ -1028,6 +1013,83 @@ class Local(Runner):
     def stop(self):
         # No explicit close-out required (so far).
         pass
+
+
+class StreamWatcher(threading.local):
+    """
+    A class whose subclasses may act on seen stream data from subprocesses.
+
+    Subclasses must exhibit the following API; see `Responder` for a concrete
+    example.
+
+    * `__init__` is simply the parameterization & state initialization vector.
+      Subclasses may do whatever they need here, as long as they remember to
+      call `super`, which does some basic bookkeeping.
+    * `submit` must accept the entire current contents of the stream being
+      watched, as a Unicode string, and may optionally return an iterable of
+      Unicode strings (or act as a generator iterator, i.e. multiple calls to
+      ``yield <unicode string>``), which will each be written to the
+      subprocess' standard input.
+
+    .. note::
+        `StreamWatcher` subclasses exist in part to enable state tracking, such
+        as detecting when a submitted password didn't work & erroring (or
+        prompting a user, or etc). Such bookkeeping isn't easily achievable
+        with simple callback functions.
+
+    .. note::
+        `StreamWatcher` subclasses `threading.local` so that their instances
+        can be used to 'watch' both subprocess stdout and stderr in separate
+        threads.
+    """
+    def submit(self, stream):
+        """
+        Act on ``stream`` data, potentially returning responses.
+
+        :param unicode stream:
+            All data read on this stream since the beginning of the session.
+
+        :returns:
+            An iterable of Unicode strings (which may be empty).
+        """
+        raise NotImplementedError
+
+
+class Responder(StreamWatcher):
+    """
+    A parameterizable object that submits responses to specific patterns.
+
+    Commonly used to implement password auto-responds for things like ``sudo``.
+    """
+
+    def __init__(self, pattern, response):
+        """
+        Imprint this `Responder` with necessary parameters.
+
+        :param pattern:
+            A raw string (e.g. ``r"\[sudo\] password for .*:"``) which will be
+            turned into a regular expression.
+
+        :param response:
+            The string to submit to the subprocess' stdin when ``pattern`` is
+            detected.
+        """
+        # TODO: precompile the keys into regex objects
+        self.pattern = pattern
+        self.response = response
+        self.index = 0
+
+    def submit(self, stream):
+        # Only look at stream contents we haven't seen yet, to avoid dupes.
+        new_ = stream[self.index:]
+        # Search, across lines if necessary
+        matches = re.findall(self.pattern, new_, re.S)
+        # Update seek index if we've matched
+        if matches:
+            self.index += len(new_)
+        # Iterate over findall() response in case >1 match occurred.
+        for _ in matches:
+            yield self.response
 
 
 class Result(object):
