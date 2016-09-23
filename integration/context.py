27fe05d52b162b3e5cf9fd94fd7717fe64ae7439
Jeff Forcier <jeff@bitprophet.org>
2016-09-23 13:43:28 -0700

    Let's see if this works

--- integration/context.py
+++ integration/context.py
@@ -8,13 +8,12 @@ from invoke import Context, Config
 class Context_(Spec):
     class sudo:
         def base_case(self):
+            # NOTE: Assumes a user whose password is 'mypass' has been created
+            # & added to passworded (not passwordless) sudo configuration; and
+            # that this user is the one running the test suite. Only for
+            # running on Travis, basically.
             if not os.environ.get('TRAVIS', False):
                 skip()
-            # NOTE: Assumes 'testuser:mypass' has been created & added to
-            # passworded (not passwordless) sudo configuration; and assumes
-            # that the user RUNNING the tests DOES have passwordless sudo.
-            # I.e., travis-ci.
             config = Config(overrides={'sudo': {'password': 'mypass'}})
-            cmd = 'sudo -u sudouser sudo whoami'
-            result = Context(config=config).sudo(cmd, hide=True)
+            result = Context(config=config).sudo('whoami', hide=True)
             eq_(result.stdout.strip(), 'root')
