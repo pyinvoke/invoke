455b4d737a9ca9a6eef49186b17411f76683d36b
Jeff Forcier <jeff@bitprophet.org>
2016-09-22 17:10:41 -0700

    I'm tired

--- integration/context.py
+++ integration/context.py
@@ -1,12 +1,12 @@
 from spec import Spec, eq_
 
-from invoke import Context
+from invoke import Context, Config
 
 
 class Context_(Spec):
     class sudo:
         def base_case(self):
             # TODO: create passworded sudo user
-            config = {'sudo': {'password': ''}}
+            config = Config(overrides={'sudo': {'password': ''}})
             result = Context(config=config).sudo('whoami')
             eq_(result.stdout.strip(), 'root')
