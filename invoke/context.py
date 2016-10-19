4db876ca060d869351caf8fb624f8211f840eb3f
Jeff Forcier <jeff@bitprophet.org>
2016-10-19 15:18:19 -0700

    Implement mapping-based mock results

--- invoke/context.py
+++ invoke/context.py
@@ -211,20 +211,33 @@ class MockContext(Context):
     # worth. Maybe in situations where Context grows a _lot_ of methods (e.g.
     # in Fabric 2; though Fabric could do its own sub-subclass in that case...)
 
-    def run(self, *args, **kwargs):
-        if not hasattr(self, '_run'):
-            raise NotImplementedError # We weren't given anything to mock
+    def _yield_result(self, attname, command):
+        if not hasattr(self, attname):
+            raise NotImplementedError
+        value = getattr(self, attname)
+        # TODO: thought there's a 'better' 2x3 DictType or w/e, but can't find
+        if isinstance(value, dict):
+            if hasattr(value[command], '__iter__'):
+                result = value[command].pop(0)
+            elif isinstance(value[command], Result):
+                result = value.pop(command)
+        elif hasattr(value, '__iter__'):
+            result = value.pop(0)
+        elif isinstance(value, Result):
+            result = value
+            delattr(self, attname)
+        return result
+
+    def run(self, command, *args, **kwargs):
         # TODO: perform more convenience stuff associating args/kwargs with the
         # result? E.g. filling in .command, etc? Possibly useful for debugging
         # if one hits unexpected-order problems with what they passed in to
         # __init__.
-        return self._run.pop(0)
+        return self._yield_result('_run', command)
 
-    def sudo(self, *args, **kwargs):
+    def sudo(self, command, *args, **kwargs):
         # TODO: this completely nukes the top-level behavior of sudo(), which
         # could be good or bad, depending. Most of the time I think it's good.
         # No need to supply dummy password config, etc.
-        if not hasattr(self, '_sudo'):
-            raise NotImplementedError # We weren't given anything to mock
         # TODO: see the TODO from run() re: injecting arg/kwarg values
-        return self._sudo.pop(0)
+        return self._yield_result('_sudo', command)
