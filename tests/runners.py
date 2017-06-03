ce67cfc9b23093d0b2f6516945e519fab468b32e
Paul Healy <lmiphay@gmail.com>
2017-06-03 20:02:24 +0100

    #344 allow limiting stdout/err capture

--- tests/runners.py
+++ tests/runners.py
@@ -145,6 +145,21 @@ class Runner_:
             assert r.stdout == "stuff"
             assert sys.stdout.getvalue() == ""
 
+    class capture_buffer_size:
+        @trap
+        def limits_capture(self):
+            runner = self._runner(out='x'*99 + 'y'*40, run={'capture_buffer_size': 40})
+            eq_(runner.run(_).stdout, 'y'*40)
+
+        @trap
+        def fails_when_too_small(self):
+            try:
+                self._runner(out='abc', run={'capture_buffer_size': 3}).run(_)
+            except ValueError as e:
+                eq_(str(e), 'capture_buffer_size is too short to match prompt')
+            else:
+                assert False, "Did not raise ValueError too small a buffer"
+
     class pty:
         def pty_defaults_to_off(self):
             assert self._run(_).pty is False
