da1b91ec08e2319001b0501561ac99da60f6d2ea
Jeff Forcier <jeff@bitprophet.org>
2017-07-24 16:05:37 -0700

    Basic failing tests re: Loader getting/using config object

--- tests/loader.py
+++ tests/loader.py
@@ -4,6 +4,7 @@ import sys
 
 from spec import Spec, eq_, raises
 
+from invoke import Config
 from invoke.loader import Loader, FilesystemLoader as FSLoader
 from invoke.collection import Collection
 from invoke.exceptions import CollectionNotFound
@@ -24,6 +25,16 @@ class _BasicLoader(Loader):
 
 
 class Loader_(Spec):
+    def exhibits_default_config_object(self):
+        loader = _BasicLoader()
+        assert isinstance(loader.config, Config)
+        assert loader.config.tasks.collection_name == 'tasks'
+
+    def may_configure_config_via_constructor(self):
+        config = Config({'tasks': {'collection_name': 'mytasks'}})
+        loader = _BasicLoader(config=config)
+        assert loader.config.tasks.collection_name == 'mytasks'
+
     def adds_module_parent_dir_to_sys_path(self):
         # Crummy doesn't-explode test.
         _BasicLoader().load('namespacing')
@@ -45,6 +56,20 @@ class Loader_(Spec):
         # make sure it doesn't explode
         loader.load('package')
 
+    def load_name_defaults_to_config_tasks_collection_name(self):
+        "load() name defaults to config.tasks.collection_name"
+        class MockLoader(_BasicLoader):
+            def find(self, name):
+                # Sanity
+                assert name == 'simple_ns_list'
+                return super(MockLoader, self).find(name)
+        config = Config({'tasks': {'collection_name': 'simple_ns_list'}})
+        loader = MockLoader(config=config)
+        # More sanity: expect a task from simple_ns_list (and not 'foo' from
+        # _support/tasks.py)
+        mod = loader.load()
+        assert 'z_toplevel' in mod
+
 
 class FilesystemLoader_(Spec):
     def setup(self):
@@ -53,12 +78,16 @@ class FilesystemLoader_(Spec):
     def discovery_start_point_defaults_to_cwd(self):
         eq_(FSLoader().start, os.getcwd())
 
-    def start_point_is_configurable(self):
+    def exposes_start_point_as_attribute(self):
+        eq_(FSLoader().start, os.getcwd())
+
+    def start_point_is_configurable_via_kwarg(self):
         start = '/tmp/'
         eq_(FSLoader(start=start).start, start)
 
-    def exposes_start_point_as_attribute(self):
-        eq_(FSLoader().start, os.getcwd())
+    def start_point_is_configurable_via_config(self):
+        config = Config({'tasks': {'search_root': 'nowhere'}})
+        eq_(FSLoader(config=config).start, 'nowhere')
 
     def returns_collection_object_if_name_found(self):
         result = self.l.load('foo')
