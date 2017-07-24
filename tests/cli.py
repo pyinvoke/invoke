8504e67fb7e8718f3c30627c7d9191f9058f45fb
Jeff Forcier <jeff@bitprophet.org>
2017-07-24 14:51:51 -0700

    Start TDD for dashed task/coll names

--- tests/cli.py
+++ tests/cli.py
@@ -37,7 +37,7 @@ class CLIParsing(Spec):
         return self._parser().parse_argv(argstr.split())
 
     def _compare(self, invoke, flagname, value):
-        invoke = "my_task " + invoke
+        invoke = "my-task " + invoke
         result = self._parse(invoke)
         eq_(result[0].args[flagname].value, value)
 
@@ -57,81 +57,81 @@ class CLIParsing(Spec):
         self._compare('--no-true-bool', 'true_bool', False)
 
     def namespaced_task(self):
-        self._compare_names("sub_coll.sub_task", "sub_coll.sub_task")
+        self._compare_names("sub-coll.sub-task", "sub_coll.sub_task")
 
     def aliases(self):
         self._compare_names("my-task27", "my-task2")
 
     def subcollection_aliases(self):
-        self._compare_names("sub_coll.other", "sub_coll.sub_task")
+        self._compare_names("sub-coll.other", "sub_coll.sub_task")
 
     def subcollection_default_tasks(self):
-        self._compare_names("sub_coll", "sub.sub_task")
+        self._compare_names("sub-coll", "sub_coll.sub_task")
 
     def boolean_args(self):
-        "my_task --boolean"
+        "my-task --boolean"
         self._compare("--boolean", 'boolean', True)
 
     def flag_then_space_then_value(self):
-        "my_task --mystring foo"
+        "my-task --mystring foo"
         self._compare("--mystring foo", 'mystring', 'foo')
 
     def flag_then_equals_sign_then_value(self):
-        "my_task --mystring=foo"
+        "my-task --mystring=foo"
         self._compare("--mystring=foo", 'mystring', 'foo')
 
     def short_boolean_flag(self):
-        "my_task -b"
+        "my-task -b"
         self._compare("-b", 'b', True)
 
     def short_flag_then_space_then_value(self):
-        "my_task -s value"
+        "my-task -s value"
         self._compare("-s value", 's', 'value')
 
     def short_flag_then_equals_sign_then_value(self):
-        "my_task -s=value"
+        "my-task -s=value"
         self._compare("-s=value", 's', 'value')
 
     def short_flag_with_adjacent_value(self):
-        "my_task -svalue"
-        r = self._parse("my_task -svalue")
+        "my-task -svalue"
+        r = self._parse("my-task -svalue")
         eq_(r[0].args.s.value, 'value')
 
     def _flag_value_task(self, value):
-        r = self._parse("my_task -s {0} my_task2".format(value))
+        r = self._parse("my-task -s {0} my-task2".format(value))
         eq_(len(r), 2)
         eq_(r[0].name, 'my_task')
         eq_(r[0].args.s.value, value)
         eq_(r[1].name, 'my_task2')
 
     def flag_value_then_task(self):
-        "my_task -s value my_task2"
+        "my-task -s value my-task2"
         self._flag_value_task('value')
 
     def flag_value_same_as_task_name(self):
-        "my_task -s my_task2 my_task2"
-        self._flag_value_task('my_task2')
+        "my-task -s my-task2 my-task2"
+        self._flag_value_task('my-task2')
 
     def three_tasks_with_args(self):
-        "my_task --boolean my_task3 --mystring foo my_task2"
-        r = self._parse("my_task --boolean my_task3 --mystring foo my_task2")
+        "my-task --boolean my-task3 --mystring foo my-task2"
+        r = self._parse("my-task --boolean my-task3 --mystring foo my-task2")
         eq_(len(r), 3)
         eq_([x.name for x in r], ['my_task', 'my_task3', 'my_task2'])
         eq_(r[0].args.boolean.value, True)
         eq_(r[1].args.mystring.value, 'foo')
 
     def tasks_with_duplicately_named_kwargs(self):
-        "my_task --mystring foo my_task3 --mystring bar"
-        r = self._parse("my_task --mystring foo my_task3 --mystring bar")
+        "my-task --mystring foo my-task3 --mystring bar"
+        r = self._parse("my-task --mystring foo my-task3 --mystring bar")
         eq_(r[0].name, 'my_task')
         eq_(r[0].args.mystring.value, 'foo')
         eq_(r[1].name, 'my_task3')
         eq_(r[1].args.mystring.value, 'bar')
 
     def multiple_short_flags_adjacent(self):
-        "my_task -bv (and inverse)"
+        "my-task -bv (and inverse)"
         for args in ('-bv', '-vb'):
-            r = self._parse("my_task {0}".format(args))
+            r = self._parse("my-task {0}".format(args))
             a = r[0].args
             eq_(a.b.value, True)
             eq_(a.v.value, True)
