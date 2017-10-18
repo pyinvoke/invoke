9bf21f967f65862da632c25bf37e31aa18c8a871
Jeff Forcier <jeff@bitprophet.org>
2017-10-18 14:51:48 -0700

    Welp this earlier test explains why shit was broke ugh

--- tests/parser/parser.py
+++ tests/parser/parser.py
@@ -354,7 +354,9 @@ class Parser_(Spec):
 
         def given_N_times_becomes_list_of_len_N(self):
             expected = ['foo', 'bar', 'biz']
-            assert self._parse('--mylist', 'foo', 'bar', 'biz') == expected
+            assert self._parse(
+                '--mylist', 'foo', '--mylist', 'bar', '--mylist', 'biz'
+            ) == expected
 
         def iterables_work_correctly_outside_a_vacuum(self):
             # Undetected bug where I was primarily focused on the -vvv use
