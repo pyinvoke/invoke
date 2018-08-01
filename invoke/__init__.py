b99ecb2029f5726dedd0bb2e10359439c0ae06a7
Jeff Forcier <jeff@bitprophet.org>
2018-07-31 17:32:45 -0700

    Thank god for that dynamic init test

--- invoke/__init__.py
+++ invoke/__init__.py
@@ -14,6 +14,7 @@ from .exceptions import (  # noqa
     UncastableEnvVar,
     UnexpectedExit,
     UnknownFileType,
+    UnpicklableConfigMember,
     WatcherError,
 )
 from .executor import Executor  # noqa
