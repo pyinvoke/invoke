e4dde45f292367c6e91f3879314ad272727c9748
Ludovico Bianchi <me@ludob.com>
2018-09-25 15:09:36 -0700

    satisfy flake8 compatibility

--- invoke/context.py
+++ invoke/context.py
@@ -364,7 +364,8 @@ class Context(DataProxy):
 
         .. versionadded:: 1.0
         """
-        # NOTE this seems to be the simplest/most universal way to implement this functionality
+        # NOTE this seems to be the simplest/most universal way to implement
+        # this functionality
         path = str(path)
         self.command_cwds.append(path)
         yield
