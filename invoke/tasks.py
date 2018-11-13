d0e8fa2c541682af1bf3640d9be80b7586555da4
Jeff Forcier <jeff@bitprophet.org>
2018-11-13 13:58:12 -0800

    Stars to dashes

--- invoke/tasks.py
+++ invoke/tasks.py
@@ -263,44 +263,44 @@ def task(*args, **kwargs):
     specified. Otherwise, the following keyword arguments are allowed in the
     parenthese'd form:
 
-    * ``name``: Default name to use when binding to a `.Collection`. Useful for
+    - ``name``: Default name to use when binding to a `.Collection`. Useful for
       avoiding Python namespace issues (i.e. when the desired CLI level name
       can't or shouldn't be used as the Python level name.)
-    * ``aliases``: Specify one or more aliases for this task, allowing it to be
+    - ``aliases``: Specify one or more aliases for this task, allowing it to be
       invoked as multiple different names. For example, a task named ``mytask``
       with a simple ``@task`` wrapper may only be invoked as ``"mytask"``.
       Changing the decorator to be ``@task(aliases=['myothertask'])`` allows
       invocation as ``"mytask"`` *or* ``"myothertask"``.
-    * ``positional``: Iterable overriding the parser's automatic "args with no
+    - ``positional``: Iterable overriding the parser's automatic "args with no
       default value are considered positional" behavior. If a list of arg
       names, no args besides those named in this iterable will be considered
       positional. (This means that an empty list will force all arguments to be
       given as explicit flags.)
-    * ``optional``: Iterable of argument names, declaring those args to
+    - ``optional``: Iterable of argument names, declaring those args to
       have :ref:`optional values <optional-values>`. Such arguments may be
       given as value-taking options (e.g. ``--my-arg=myvalue``, wherein the
       task is given ``"myvalue"``) or as Boolean flags (``--my-arg``, resulting
       in ``True``).
-    * ``iterable``: Iterable of argument names, declaring them to :ref:`build
+    - ``iterable``: Iterable of argument names, declaring them to :ref:`build
       iterable values <iterable-flag-values>`.
-    * ``incrementable``: Iterable of argument names, declaring them to
+    - ``incrementable``: Iterable of argument names, declaring them to
       :ref:`increment their values <incrementable-flag-values>`.
-    * ``default``: Boolean option specifying whether this task should be its
+    - ``default``: Boolean option specifying whether this task should be its
       collection's default task (i.e. called if the collection's own name is
       given.)
-    * ``auto_shortflags``: Whether or not to automatically create short
+    - ``auto_shortflags``: Whether or not to automatically create short
       flags from task options; defaults to True.
-    * ``help``: Dict mapping argument names to their help strings. Will be
+    - ``help``: Dict mapping argument names to their help strings. Will be
       displayed in ``--help`` output.
-    * ``depends_on``, ``afterwards``: Task object (or iterable of same) to
+    - ``depends_on``, ``afterwards``: Task object (or iterable of same) to
       execute prior to, or after, the wrapped task whenever it is executed.
       (See :ref:`how-tasks-run`.)
-    * ``pre``, ``post``: Legacy aliases for ``depends_on`` and ``afterwards``,
+    - ``pre``, ``post``: Legacy aliases for ``depends_on`` and ``afterwards``,
       respectively.
-    * ``autoprint``: Boolean determining whether to automatically print this
+    - ``autoprint``: Boolean determining whether to automatically print this
       task's return value to standard output when invoked directly via the CLI.
       Defaults to False.
-    * ``klass``: Class to instantiate/return. Defaults to `.Task`.
+    - ``klass``: Class to instantiate/return. Defaults to `.Task`.
 
     If any non-keyword arguments are given, they are taken as the value of the
     ``pre`` kwarg for convenience's sake. (It is an error to give both
