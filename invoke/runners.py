f971cc67339086a093200486ac52f577a9c15a3b
Jeff Forcier <jeff@bitprophet.org>
2020-01-10 10:36:45 -0500

    Replace a TODO re: exec

--- invoke/runners.py
+++ invoke/runners.py
@@ -1031,7 +1031,8 @@ class Local(Runner):
                 # Use execve for bare-minimum "exec w/ variable # args + env"
                 # behavior. No need for the 'p' (use PATH to find executable)
                 # for now.
-                # TODO: see if subprocess is using equivalent of execvp...
+                # NOTE: stdlib subprocess (actually its posix flavor, which is
+                # written in C) uses either execve or execv, depending.
                 os.execve(shell, [shell, "-c", command], env)
         else:
             self.process = Popen(
