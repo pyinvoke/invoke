67bc038bcb35de158d5f4dca4346fca5ab344e47
Jeff Forcier <jeff@bitprophet.org>
2022-01-21 18:24:40 -0500

    Switch from Travis to CircleCI

--- integration/context.py
+++ integration/context.py
@@ -1,19 +1,19 @@
-import os
-
-from pytest import skip
-
 from invoke import Context, Config
+from invocations import ci as ci_mod
 
 
 class Context_:
     class sudo:
         def base_case(self):
-            # NOTE: Assumes a user whose password is 'mypass' has been created
-            # & added to passworded (not passwordless) sudo configuration; and
-            # that this user is the one running the test suite. Only for
-            # running on Travis, basically.
-            if not os.environ.get("TRAVIS", False):
-                skip()
-            config = Config({"sudo": {"password": "mypass"}})
-            result = Context(config=config).sudo("whoami", hide=True)
-            assert result.stdout.strip() == "root"
+            c = Context()
+            # Grab CI-oriented sudo user/pass direct from invocations.ci
+            # TODO: might be nice to give Collection a way to get a Config
+            # object direct, instead of a dict?
+            ci_conf = Config(ci_mod.ns.configuration()).ci.sudo
+            user = ci_conf.user
+            c.config.sudo.password = ci_conf.password
+            # Safety 1: ensure configured user even exists
+            assert c.run("id {}".format(user), warn=True)
+            # Safety 2: make sure we ARE them (and not eg root already)
+            assert c.run("whoami", hide=True).stdout.strip() == user
+            assert c.sudo("whoami", hide=True).stdout.strip() == "root"
--- tasks.py
+++ tasks.py
@@ -3,7 +3,7 @@ import os
 from invoke import Collection, task, Exit
 from invoke.util import LOG_FORMAT
 
-from invocations import travis, checks
+from invocations import ci, checks
 from invocations.docs import docs, www, sites, watch_docs
 from invocations.pytest import coverage as coverage_, test as test_
 from invocations.packaging import vendorize, release
@@ -68,15 +68,22 @@ def integration(c, opts=None, pty=True):
 
 
 @task
-def coverage(c, report="term", opts=""):
+def coverage(c, report="term", opts="", codecov=False):
     """
     Run pytest in coverage mode. See `invocations.pytest.coverage` for details.
     """
     # Use our own test() instead of theirs.
-    # TODO: allow coverage() to just look up the nearby-by-namespace-attachment
-    # test() instead of hardcoding its own test or doing it this way with an
-    # arg.
-    return coverage_(c, report=report, opts=opts, tester=test)
+    # Also add integration test so this always hits both.
+    # (Not regression, since that's "weird" / doesn't really hit any new
+    # coverage points)
+    coverage_(
+        c,
+        report=report,
+        opts=opts,
+        tester=test,
+        additional_testers=[integration],
+        codecov=codecov,
+    )
 
 
 @task
@@ -102,23 +109,18 @@ ns = Collection(
     docs,
     sites,
     watch_docs,
-    travis,
+    ci,
     checks.blacken,
 )
 ns.configure(
     {
         "blacken": {
-            # Skip the vendor directory and the (Travis-only) alt venv when
-            # blackening.
+            # Skip vendor, build dirs when blackening.
             # TODO: this is making it seem like I really do want an explicit
             # arg/conf-opt in the blacken task for "excluded paths"...ha
-            "find_opts": "-and -not \( -path './invoke/vendor*' -or -path './alt_env*' -or -path './build*' \)"  # noqa
+            "find_opts": "-and -not \( -path './invoke/vendor*' -or -path './build*' \)"  # noqa
         },
         "tests": {"logformat": LOG_FORMAT, "package": "invoke"},
-        "travis": {
-            "sudo": {"user": "sudouser", "password": "mypass"},
-            "black": {"version": "18.6b4"},
-        },
         "packaging": {
             "sign": True,
             "wheel": True,
