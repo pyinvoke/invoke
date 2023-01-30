380e93f64537a1dad43b99b39ac7b7fdf3f676b1
Jesse P. Johnson <jpj6652@gmail.com>
2023-01-29 20:23:09 -0500

    ci: remove travis cruft

--- invoke/_version.py
+++ invoke/_version.py
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 __version_info__ = (2, 0, 0)
+=======
+__version_info__ = (1, 4, 1)
+>>>>>>> 38d7debf (ci: resolved some errors for sites and docs)
 __version__ = ".".join(map(str, __version_info__))
