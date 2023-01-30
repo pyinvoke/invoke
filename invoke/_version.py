a91e661d89f777b16587ddd1a6555aa06e1d5dcb
Jesse P. Johnson <jpj6652@gmail.com>
2023-01-29 20:26:11 -0500

    build: update poetry implementation

--- invoke/_version.py
+++ invoke/_version.py
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
 __version_info__ = (2, 0, 0)
 __version__ = ".".join(map(str, __version_info__))
-=======
-from importlib_metadata import version  # type: ignore
-
-__version__ = version(__package__)
-__version_info__ = tuple(map(int, __version__.split('.')))
->>>>>>> 2093cf98 (github editor)
